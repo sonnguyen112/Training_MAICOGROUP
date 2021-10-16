d7537e9af88681b3ed50b1e1ef73e45189ceee3c
sonnguyen112 <80495667+sonnguyen112@users.noreply.github.com>
2021-10-16 12:24:40 +0700

    gradient descent

--- linear_regression_vs_GD.py
+++ linear_regression_vs_GD.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn import linear_model
 import seaborn as sns
 import matplotlib.pyplot as plt
 
